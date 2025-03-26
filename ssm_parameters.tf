3d88ea11b8b4e9592fe039403ea3f083d0b7e95d
Afraz Ahmadzadeh <afrazo@proton.me>
2025-03-26 14:22:17 +0100

    Match patch versions for SSM Parameter module

--- ssm_parameters.tf
+++ ssm_parameters.tf
@@ -1,6 +1,6 @@
 module "ssm_parameters" {
   source  = "guidion-digital/helper-ssm-parameters/aws"
-  version = "0.1.0"
+  version = "~> 0.1.0"
 
   project          = var.project
   application_name = var.application_name
