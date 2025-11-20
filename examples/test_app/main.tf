5b1bb42d2eb1e0cb5d42c3b50ba271586fd19d8f
Afraz Ahmadzadeh <afrazo@proton.me>
2025-11-20 11:27:35 +0100

    Now we can test more resources

--- examples/test_app/main.tf
+++ examples/test_app/main.tf
@@ -2,7 +2,7 @@ variable "application_name" { default = "test-app" }
 variable "project" { default = "constr" }
 variable "stage" { default = "development" }
 variable "grafana_promtail_lambda_arn" { default = "arn:aws:lambda:eu-central-1:000000000000:function:grafana-promtail-lambda" }
-# Left for edification, not used in tests
+# Commented out because it's not used for tests
 # variable "vpc_config" {
 #   default = {
 #     vpc_cidr                = "10.126.2.0/24"
@@ -28,7 +28,7 @@ module "shared_resources_x" {
   # Optional variables
   # If provided, matching resources will be created.
 
-  # Left for edification, not used in tests
+  # Commented out because it's not used for tests
   # Some resources such as RDS require the creation of a VPC, so vpc_config must also be passed if creating those or they will be skipped
   # vpc_config = var.vpc_config
 
@@ -66,32 +66,31 @@ module "shared_resources_x" {
     }
   }
 
-  # Not supported by Localstack, but this is how it's used :)
-  #   dynamodb_tables = {
-  #     "test-table" = {
-  #       attributes = [
-  #         { name = "id", type = "N" },
-  #         { name = "createdAt", type = "S" },
-  #         { name = "gsi1", type = "N" }
-  #       ],
-  #       hash_key  = "id",
-  #       range_key = "createdAt",
-  #
-  #       global_secondary_indexes = [
-  #         {
-  #           name               = "createdAtIndex",
-  #           hash_key           = "createdAt",
-  #           range_key          = "gsi1",
-  #           non_key_attributes = ["id"],
-  #           autoscaling = {
-  #             read_max_capacity = 25
-  #           }
-  #         }
-  #       ]
-  #
-  #       ttl_attribute_name = "expiresAt",
-  #     }
-  #   }
+  dynamodb_tables = {
+    "test-table" = {
+      attributes = [
+        { name = "id", type = "N" },
+        { name = "createdAt", type = "S" },
+        { name = "gsi1", type = "N" }
+      ],
+      hash_key  = "id",
+      range_key = "createdAt",
+
+      global_secondary_indexes = [
+        {
+          name               = "createdAtIndex",
+          hash_key           = "createdAt",
+          range_key          = "gsi1",
+          non_key_attributes = ["id"],
+          autoscaling = {
+            read_max_capacity = 25
+          }
+        }
+      ]
+
+      ttl_attribute_name = "expiresAt",
+    }
+  }
 
   sqs_queues = {
     "test-queue" = {
@@ -106,36 +105,35 @@ module "shared_resources_x" {
     }
   }
 
-  # Not supported by Localstack, but this is how it's used :)
-  # elasticache = {
-  #   "memcached-01" = {
-  #     application_name = var.application_name
-  #     project          = var.project
-  #     stage            = var.stage
-  #     engine           = "memcached"
-  #     engine_version   = "1.6.17"
-  #     node_type        = "cache.t3.micro"
-  #     az_mode          = "single-az"
-  #   }
-  # }
+  elasticache = {
+    "memcached-01" = {
+      application_name = var.application_name
+      project          = var.project
+      stage            = var.stage
+      engine           = "memcached"
+      engine_version   = "1.6.17"
+      node_type        = "cache.t3.micro"
+      az_mode          = "single-az"
+    }
+  }
 
-  # 30 minutes is too long for tests to run! but this is how it's used :)
-  #   rds_instances = {
-  #     "test-db" = {
-  #       username        = "test"
-  #       allow_vpc_cidr  = true
-  #       multi_az        = false
-  #       purge_on_delete = true
-  #
-  #       proxy_settings = {
-  #         enabled = true
-  #       }
-  #
-  #       replica_settings = {
-  #         enabled = false
-  #       }
-  #     }
-  #   }
+  rds_instances = {
+    "test-db" = {
+      username = "test"
+      # Commented out because it's not used for tests
+      # allow_vpc_cidr  = true
+      multi_az        = false
+      purge_on_delete = true
+
+      proxy_settings = {
+        enabled = true
+      }
+
+      replica_settings = {
+        enabled = false
+      }
+    }
+  }
 }
 
 output "secrets" {
@@ -166,9 +164,10 @@ output "rds" {
   value = module.shared_resources_x.rds
 }
 
-output "vpc" {
-  value = module.shared_resources_x.vpc
-}
+# Commented out because it's not used for tests
+# output "vpc" {
+#   value = module.shared_resources_x.vpc
+# }
 
 output "rds_proxy_connect_role_arns" {
   value = module.shared_resources_x.rds_proxy_connect_role_arns
